9b7c1d88fab2d5e866ce2254f1ccadb8da8296e8
BarKabal <bkabala00@gmail.com>
2022-06-12 17:05:14 +0200

    ReadHash.py skończone

--- src/ReadHash.py
+++ src/ReadHash.py
@@ -5,14 +5,28 @@ from src.RainbowTable import gen_pwd
 def findHashInTable(comm):
     request = comm.irecv(source=0)
     data = request.wait()
-    table_part = data[0]
-    hash = data[1]
+    answer = calculateHashInTable(data[0], data[1], data[2], data[3])
+    comm.isend(answer, dest=0)
 
-    # 1.check if string you got from hash with reduction#999 is in last column (at the ends of rows)
-    # 2.if not then check with reduction#998 then hash, then reduction#999
-    # 3.continue in the same pattern until you go arrive at reduction#0 if you didn't find the answer, the attack failed
-    # 4.if you find the answer somewhere along the way get the start of the chain where you found the hash
-    # 5.remake the chain in its entirety until you find the pwd that makes the hash
+
+def calculateHashInTable(table_column_start, table_column_end, target_hash, length):
+    for i in range(999, -1, -1):
+        string = ""
+        working_hash = target_hash
+        index = -1
+        for j in range(i, 1000):
+            string = hash_to_pwd(working_hash, length, j)
+            working_hash = SHA256.SHA256Hash(bytes(string, 'utf-8')).hexdigest()
+        if string in table_column_end:
+            index = table_column_end.index(string)
+        if index != -1:
+            string = table_column_start[index]
+            for k in range(0, 1000):
+                working_hash = SHA256.SHA256Hash(bytes(string, 'utf-8')).hexdigest()
+                if working_hash == target_hash:
+                    return string
+                string = hash_to_pwd(working_hash, length, k)
+    return ""
 
 
 def hash_to_pwd(h, length, index):
