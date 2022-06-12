9491681d46b271fbd74d4ab9d49fe195b002d499
BarKabal <bkabala00@gmail.com>
2022-06-12 17:33:45 +0200

    ReadManager.py skończone

--- src/ReadManager.py
+++ src/ReadManager.py
@@ -1,4 +1,5 @@
 import string
+import re
 
 
 def collectAnswer(n_processes, comm):
@@ -17,14 +18,24 @@ def collectAnswer(n_processes, comm):
 
 def read_table_file(file_name: string):
     file = open(file_name, "r")
-    return []
+    lines = file.readlines()
+    start = []
+    end = []
+    for i, line in lines:
+        if i % 2 == 0:
+            start.append(line)
+        else:
+            end.append(line)
+    return [start, end]
 
 
 def manageReadingTable(n_processes, comm):
     path = input("Please provide the file path: ")
-    pwd = input("Please type the hash you are looking for: ")
+    target_hash = input("Please type the hash you are looking for: ")
     table = read_table_file(path)
-    data = [table, pwd]
+    length = "".join(c for c in path.split("_")[2] if c.isdigit())
+    print(length)
+    data = [table[0], table[1], target_hash, int(length)]
     for i in range(1, n_processes):
         comm.isend(data, dest=i)    # this should split data so that only a part is passed
     collectAnswer(n_processes, comm)
